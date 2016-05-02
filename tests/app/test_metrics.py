9ebbb6395f4accf89475107f971ef4f65557c92e
Roman Mohr <rmohr@redhat.com>
2016-05-02 11:45:51 +0200

    Set up test infrastructure
    Integrate py.test and write first test for the metrics WSGI endpoints.

---
+++ tests/app/test_metrics.py
@@ -0,0 +1,25 @@
+from vadvisor.app.rest import make_rest_app
+import pytest
+import webtest
+
+
+@pytest.fixture
+def _app():
+    return make_rest_app()
+
+
+@pytest.fixture
+def app(_app):
+    return webtest.TestApp(_app)
+
+
+def test_metrics(app):
+    resp = app.get("/metrics")
+    assert resp.status_int == 200
+    assert "process_virtual_memory_bytes" in resp
+
+
+def test_vms(app):
+    resp = app.get("/v1.0/vms")
+    assert resp.status_int == 200
+    assert resp.json == []
