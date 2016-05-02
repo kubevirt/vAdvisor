a61ae692afc807e54ebd95de893e0ef00801308c
Roman Mohr <rmohr@redhat.com>
2016-05-02 15:42:05 +0200

    Collect disk and network metrics
    Collect and expose disk and network metrics.

--- vadvisor/app/prometheus.py
+++ vadvisor/app/prometheus.py
@@ -12,41 +12,56 @@ class Subtree:
             self._elements[element.field] = element
         self.field = field
 
+    def process(self, label_keys, labels, domainStats):
+        for field, element in six.iteritems(self._elements):
+            if field in domainStats:
+                for metric in element.process(label_keys, labels, domainStats[field]):
+                    yield metric
+
+
+class Tree(Subtree):
+
+    def __init__(self, label_keys, elements):
+        Subtree.__init__(self, None, elements)
+        self._label_keys = label_keys
+
     def process(self, labels, domainStats):
         for field, element in six.iteritems(self._elements):
             if field in domainStats:
-                for metric in element.process(labels, domainStats[field]):
+                for metric in element.process(self._label_keys, labels, domainStats[field]):
                     yield metric
 
 
 class Metric:
-    _labels = ['name', 'uuid']
 
     def __init__(self, name, field, description):
         self.field = field
         self.name = name
         self.description = description
+        self.metric = None
 
 
 class Gauge(Metric):
 
-    def process(self, labels, value):
-        gauge = GaugeMetricFamily(self.name, self.description, labels=self._labels)
-        gauge.add_metric(labels, value)
-        yield gauge
+    def process(self, label_keys, labels, value):
+        if not self.metric:
+            self.metric = GaugeMetricFamily(self.name, self.description, labels=label_keys)
+        self.metric.add_metric(labels, value)
+        yield self.metric
 
 
 class Counter(Metric):
 
-    def process(self, labels, value):
-        gauge = CounterMetricFamily(self.name, self.description, labels=self._labels)
-        gauge.add_metric(labels, value)
-        yield gauge
+    def process(self, label_keys, labels, value):
+        if not self.metric:
+            self.metric = CounterMetricFamily(self.name, self.description, labels=label_keys)
+        self.metric.add_metric(labels, value)
+        yield self.metric
 
 
 class LibvirtCollector:
 
-    _tree = Subtree(None, [
+    _vm = Tree(['uuid'], [
         Subtree('cpu', [
             Counter('vm_cpu_time', 'cpu_time', 'Overall VM CPU time'),
             Counter('vm_cpu_system_time', 'system_time', 'Overall VM System CPU time'),
@@ -57,12 +72,51 @@ class LibvirtCollector:
         ])
     ])
 
+    _interfaces = Tree(['uuid', 'interface'], [
+        Counter('vm_network_receive_bytes_total', 'rx_bytes', 'Cumulative count of bytes received'),
+        Counter('vm_network_receive_packets_total', 'rx_packets', 'Cumulative count of packets received'),
+        Counter('vm_network_receive_packages_dropped_total', 'rx_dropped', 'Cumulative count of packages dropped while receiving'),
+        Counter('vm_network_receive_errors_total', 'rx_errors', 'Cumulative count of errors encountered while receiving'),
+        Counter('vm_network_transmit_bytes_total', 'tx_bytes', 'Cumulative count of bytes transmitted'),
+        Counter('vm_network_transmit_packets_total', 'tx_packets', 'Cumulative count of packets transmitted'),
+        Counter('vm_network_transmit_packages_dropped_total', 'tx_dropped', 'Cumulative count of packages dropped while transmitting'),
+        Counter('vm_network_transmit_errors_total', 'tx_errors', 'Cumulative count of errors encountered while transmitting'),
+    ])
+
+    _disks = Tree(['uuid', 'device'], [
+        Counter('vm_disk_write_requests_total', 'wr_req', ''),
+        Counter('vm_disk_write_bytes_total', 'wr_bytes', ''),
+        Counter('vm_disk_read_requests_total', 'rd_req', ''),
+        Counter('vm_disk_read_bytes_total', 'rd_bytes', ''),
+    ])
+
+    _cpus = Tree(['uuid', 'cpu'], [
+        Counter('vm_vcpu_total', 'vcpu_time', ''),
+        Counter('vm_cpu_total', 'cpu_time', ''),
+    ])
+
     def __init__(self):
         self.collector = Collector()
 
     def collect(self):
         stats = self.collector.collect()
         for domainStats in stats:
-            labels = [domainStats['name'], domainStats['uuid']]
-            for metric in self._tree.process(labels, domainStats):
+            # VM stats
+            labels = [domainStats['uuid']]
+            for metric in self._vm.process(labels, domainStats):
                 yield metric
+            # Networking stats
+            for interface in domainStats['network']['interfaces']:
+                labels = [domainStats['uuid'], interface['name']]
+                for metric in self._interfaces.process(labels, interface):
+                    yield metric
+            # Disk stats
+            for disk in domainStats['diskio']:
+                labels = [domainStats['uuid'], disk['name']]
+                for metric in self._disks.process(labels, disk):
+                    yield metric
+            # CPU stats
+            for indx, cpu in enumerate(domainStats['cpu']['per_cpu_usage']):
+                labels = [domainStats['uuid'], str(indx)]
+                for metric in self._cpus.process(labels, cpu):
+                    yield metric
--- vadvisor/virt/collector.py
+++ vadvisor/virt/collector.py
@@ -1,4 +1,5 @@
 import libvirt
+from xml.etree import ElementTree
 
 
 class Collector:
@@ -26,13 +27,48 @@ class Collector:
             print('Failed to get list of domains')
         for domainID in domainIDs:
             domain = self._conn.lookupByID(domainID)
+            tree = ElementTree.fromstring(domain.XMLDesc())
+            interfaces = []
+            for iface in tree.findall('devices/interface/target'):
+                name = iface.get('dev')
+                ifStats = domain.interfaceStats(name)
+                interfaces.append({
+                    'name': name,
+                    'rx_bytes': ifStats[0],
+                    'rx_packets': ifStats[1],
+                    'rx_errors': ifStats[2],
+                    'rx_drops': ifStats[3],
+                    'tx_bytes': ifStats[4],
+                    'tx_packets': ifStats[5],
+                    'tx_errors': ifStats[6],
+                    'tx_dropped': ifStats[7],
+                }
+                )
+            disks = []
+            for disk in tree.findall('devices/disk/target'):
+                name = disk.get('dev')
+                diskStats = domain.blockStats(name)
+                disks.append({
+                    'name': name,
+                    'rd_req': diskStats[0],
+                    'rd_bytes': diskStats[1],
+                    'wr_req': diskStats[2],
+                    'wr_bytes': diskStats[3],
+                    'errors': diskStats[4],
+                })
             domainName = domain.name()
             domainUUID = domain.UUIDString()
-
             domainStats = {}
             domainStats['uuid'] = domainUUID
             domainStats['name'] = domainName
             domainStats['memory'] = domain.memoryStats()
-            domainStats['cpu'] = domain.getCPUStats(True)[0]
+            domainStats['cpu'] = {
+                "usage": domain.getCPUStats(True)[0],
+                "per_cpu_usage": domain.getCPUStats(False),
+            }
+            domainStats['network'] = {
+                'interfaces': interfaces
+            }
+            domainStats['diskio'] = disks
             stats.append(domainStats)
         return stats
